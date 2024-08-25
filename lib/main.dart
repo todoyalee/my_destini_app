826c70608a56ab3d41e3bfc5e81bb7b4e44496a2
Mohamed Ali Belkouri <medali.b@devnet.me>
2024-08-25 01:53:04 +0100

    feat:adding_desing+action

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_destini_app/story.dart';
+
 import 'package:my_destini_app/story_brain.dart';
 
 void main() {
@@ -14,15 +14,79 @@ class first extends StatefulWidget {
 }
 
 class _firstState extends State<first> {
+  var list = StoryBrain();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Container(
           decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage("image/image.png"))),
-          child: Column(
-            children: [Text("")],
+              image: DecorationImage(image: AssetImage("images/image.png"))),
+          padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+          constraints: BoxConstraints.expand(),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  flex: 12,
+                  child: Center(
+                    child: Text(
+                      list.getStoryTitle(),
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25.0),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Expanded(
+                  flex: 2,
+                  child: ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                    onPressed: () {
+                      list.nextStory(1);
+                    },
+                    child: Text(
+                      list.getChoice1(),
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25.0),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Visibility(
+                    visible: list.ButtonShouldBeVisible(),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.lightBlue),
+                      onPressed: () {
+                        list.nextStory(2);
+                      },
+                      child: Text(
+                        list.getChoice2(),
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25.0),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
